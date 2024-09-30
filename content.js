e74cb42b1a7fb7593af5d00c009a7a3b8e648a94
silver-shadow <107355428+silver-shadow@users.noreply.github.com>
2024-09-30 18:04:43 +0530

    Add functionality to hide 'is-shorts' elements on YouTube pages

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
 chrome.runtime.sendMessage({todo:"showPageAction"});
+
+function hideYouTubeShorts() {
+    let elements = document.querySelectorAll('[is-shorts]');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'none'; 
+    }
+}
+
+hideYouTubeShorts();
+
+setInterval(hideYouTubeShorts, 1000); 
