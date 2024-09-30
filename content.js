87a33e11d8d7797df857baf02573378a636b7746
silver-shadow <107355428+silver-shadow@users.noreply.github.com>
2024-09-30 19:25:31 +0530

    Enhance content script to hide reel shelf elements

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.runtime.sendMessage({todo:"showPageAction"});
 
 function hideYouTubeShorts() {
-    let elements = document.querySelectorAll('[is-shorts]');
+    let elements = document.querySelectorAll('[is-shorts], ytd-reel-shelf-renderer');
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.display = 'none'; 
     }
